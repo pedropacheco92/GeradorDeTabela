40eca099d0d46f223277ba27ee925772f9310edb
pedropacheco92 <pedropacheco92@outlook.com>
2015-04-22 19:02:32 -0300

    teste

---
+++ Teste/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá Mundo!");
+		System.out.println("teste git");
+	}
+
+}
